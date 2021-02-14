6484ae9af095c7e974db1b6ae359358e7429e5bf
Govindakumar181 <Govindakumar181@gmail.com>
2021-02-14 23:20:01 +0500

    lol

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello 108 999 108 World </h1>
+      <h1>Hello 108 108999108 108 World </h1>
     </div>
   );
 }
 
 export default App;
+
+/* 
+- name: GitHubAction for Yarn
+        uses: Borales/actions-yarn@v2.3.0
+        with:
+          cmd: install
+       */
